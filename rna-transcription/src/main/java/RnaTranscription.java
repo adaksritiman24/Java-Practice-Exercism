daf2090964cc2baa0f54f69ab40d740c500c178e
Sritiman Adak <adaksritiman24@gmail.com>
2022-01-10 13:55:11 +0530

    rna transcription completed

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class RnaTranscription {
+
+    private static Map<Character, Character> rnaMappings = Map.of('G','C','C','G','T','A','A','U');
+    
+    String transcribe(String dnaStrand) {
+        
+        StringBuffer RNAcomplement = new StringBuffer();
+
+        for(char strand : dnaStrand.toCharArray()){
+            RNAcomplement.append(rnaMappings.get(strand));
+        }
+        return RNAcomplement.toString();
+    }
+
+}
